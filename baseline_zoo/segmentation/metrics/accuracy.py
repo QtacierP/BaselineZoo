18c360e3852e518a27c929705a43836e48341d8b
QtacierP <11610132@mail.sustech.edu.cn>
2021-01-25 20:39:28 +0800

    in-inplementation training phase

--- baseline_zoo/segmentation/metrics/accuracy.py
+++
@@ -1,9 +0,0 @@
-import pytorch_lightning as pl
-
-# TODO
-
-class SegmentationAccuracy(pl.metrics.Accuracy):
-    def __init__(self, threshold: float, compute_on_step: bool, dist_sync_on_step: bool, process_group: Optional[Any]):
-        super().__init__(threshold=threshold, compute_on_step=compute_on_step, 
-        dist_sync_on_step=dist_sync_on_step, process_group=process_group)
-    
